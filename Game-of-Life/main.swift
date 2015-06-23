64dc4904ed3ab15ff0c63a58ce79f1c15b2c8e7e
mr-ndrw <andrew.torski@gmail.com>
2015-06-23 17:48:48 +0200

    Initial Commit

---
+++ Game-of-Life/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Game-of-Life
+//
+//  Created by Andrew Torski on 23/06/15.
+//  Copyright (c) 2015 Andrew Torski. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
